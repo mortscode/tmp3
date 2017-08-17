1a4d60fe7ab7b60815d4d28cf32a3dad0cee08b7
Scot Mortimer <scot.mortimer@gmail.com>
2017-08-17 13:22:41 -0700

    pretransforms

--- craft/config/imagerpretransform.php
+++ craft/config/imagerpretransform.php
@@ -6,96 +6,129 @@ return [
             // JPG
             [
                 'format' => 'jpg',
-                'width' => 1000,
-            ],
-            [
-                'format' => 'jpg',
-                'width' => 667,
-            ],
-            [
-                'format' => 'jpg',
-                'width' => 400,
-            ],
-            // WEBP
-            [
-                'format' => 'webp',
-                'width' => 1000,
-            ],
-            [
-                'format' => 'webp',
-                'width' => 667,
-            ],
-            [
-                'format' => 'webp',
-                'width' => 400,
-            ],
-            'defaults' => [
+                'width' => function ($asset) {
+                    if ($asset->width > $asset->height) {
+                        return 1000;
+                    } else {
+                        return 667;
+                    }
+                },
                 'position' => function ($asset) {
                     return $asset->focalPoint;
                 },
                 'ratio' => function ($asset) {
                     return $asset->width / $asset->height;
+                }
+            ],
+            [
+                'format' => 'jpg',
+                'width' => 500,
+                'position' => function ($asset) {
+                    return $asset->focalPoint;
                 },
-                'allowUpscale' => 'false',
-                'mode' => 'crop',
-                'interlace' => 'true'
+                'ratio' => function ($asset) {
+                    return $asset->width / $asset->height;
+                }
+            ],
+            // WEBP
+            [
+                'format' => 'webp',
+                'width' => function ($asset) {
+                    if ($asset->width > $asset->height) {
+                        return 1000;
+                    } else {
+                        return 667;
+                    }
+                },
+                'position' => function ($asset) {
+                    return $asset->focalPoint;
+                },
+                'ratio' => function ($asset) {
+                    return $asset->width / $asset->height;
+                }
+            ],
+            [
+                'format' => 'webp',
+                'width' => 500,
+                'position' => function ($asset) {
+                    return $asset->focalPoint;
+                },
+                'ratio' => function ($asset) {
+                    return $asset->width / $asset->height;
+                }
             ]
         ],
         'staff' => [
             [
+                'format' => 'jpg',
                 'width' => 500,
-                'format' => 'jpg'
-            ],
-            [
-                'width' => 500,
-                'format' => 'webp'
-            ],
-            'defaults' => [
                 'position' => function ($asset) {
                     return $asset->focalPoint;
                 },
-                'ratio' => 1 / 1,
-                'allowUpscale' => 'false',
-                'mode' => 'crop',
-                'interlace' => 'true'
+                'ratio' => 1 / 1
+            ],
+            [
+                'format' => 'webp',
+                'width' => 500,
+                'position' => function ($asset) {
+                    return $asset->focalPoint;
+                },
+                'ratio' => 1 / 1
             ]
         ],
         'global' => [
             [
                 'format' => 'jpg',
-                'width' => 1000
-            ],
-            [
-                'format' => 'jpg',
-                'width' => 667
-            ],
-            [
-                'format' => 'jpg',
-                'width' => 400
-            ],
-            // WEBP
-            [
-                'format' => 'webp',
-                'width' => 1000
-            ],
-            [
-                'format' => 'webp',
-                'width' => 667
-            ],
-            [
-                'format' => 'webp',
-                'width' => 400
-            ],
-            'defaults' => [
+                'width' => function ($asset) {
+                    if ($asset->width > $asset->height) {
+                        return 1000;
+                    } else {
+                        return 667;
+                    }
+                },
                 'position' => function ($asset) {
                     return $asset->focalPoint;
                 },
                 'ratio' => function ($asset) {
                     return $asset->width / $asset->height;
+                }
+            ],
+            [
+                'format' => 'jpg',
+                'width' => 400,
+                'position' => function ($asset) {
+                    return $asset->focalPoint;
                 },
-                'allowUpscale' => 'false',
-                'mode' => 'crop',
-                'interlace' => 'true'
+                'ratio' => function ($asset) {
+                    return $asset->width / $asset->height;
+                }
+            ],
+            // WEBP
+            [
+                'format' => 'webp',
+                'width' => function ($asset) {
+                    if ($asset->width > $asset->height) {
+                        return 1000;
+                    } else {
+                        return 667;
+                    }
+                },
+                'position' => function ($asset) {
+                    return $asset->focalPoint;
+                },
+                'ratio' => function ($asset) {
+                    return $asset->width / $asset->height;
+                }
+            ],
+            [
+                'format' => 'webp',
+                'width' => 400,
+                'position' => function ($asset) {
+                    return $asset->focalPoint;
+                },
+                'ratio' => function ($asset) {
+                    return $asset->width / $asset->height;
+                }
             ]
         ]
     ]
